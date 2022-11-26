84e0e304c805e98c58d56094dde73ed98fab5144
Nadya <nad.anikina@gmail.com>
2022-11-26 20:48:00 +0000

    Добавила метод заполнения второго массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array1 = new string[5] {"111", "nadi", "ura", "100", "wow"};
 string[] array2 = new string[array1.Length];
+void FillNewShortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
